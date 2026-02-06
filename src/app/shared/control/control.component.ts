85ea704e2f9a45ea4e0ab235d807a6f8a91496f6
Giuditta <giudittamarino89@gmail.com>
2026-02-06 19:24:54 +0100

    training with components

---
+++ src/app/shared/control/control.component.ts
@@ -0,0 +1,21 @@
+import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './control.component.html',
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: "control",
+    '(click)': 'onClick()'
+  }
+})
+export class ControlComponent {
+  label = input.required<string>();
+
+  onClick() {
+    console.log('on click');    
+  }
+}
