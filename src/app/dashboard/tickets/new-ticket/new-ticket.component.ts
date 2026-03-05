e69ec0d16ba120a8f5e94b73109c417648abcf8b
Giuditta <giudittamarino89@gmail.com>
2026-03-05 11:35:09 +0100

    fix error

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -11,8 +11,7 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent implements AfterViewInit {
-  @ViewCild('form') form?: ElementRef<HTMLFormElement>;
-  //prova
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
   //@Output() addTicket = new EventEmitter<{title: string, request: string}>();
   //private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
   addTicket = output<{title: string, request: string}>();
